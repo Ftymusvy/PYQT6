070faed59f671aca7380f709ef7d068297a8c2c3
Fatemeh Mousavinejhad <fatemehmosavinejad202@gmail.com>
2024-11-08 20:12:27 +0330

    QTimer

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication , QWidget, QMainWindow, QLabel ,QPushButton, QMenu,QLineEdit, QHBoxLayout
+from PyQt6.QtWidgets import QApplication , QWidget, QMainWindow, QLabel ,QPushButton, QMenu,QLineEdit, QHBoxLayout,QLCDNumber
 import sys
-from PyQt6.QtGui import QFont , QIcon ,QPixmap 
+from PyQt6.QtGui import QFont , QIcon ,QPixmap ,QMovie
+from PyQt6.QtCore import QTime,QTimer
 #QMainWndow
 #QWidget
 #QDialog
@@ -14,19 +15,19 @@ class MyWindow(QWidget):
         self.setMinimumSize(100,100)
         self.setWindowOpacity(1)
         self.setWindowTitle('Fatemeh')
-        self.label= QLabel("hello" , self)
+     #   self.label= QLabel("hello" , self)
         #self.label.move(40,40)
         #self.label.setFont(QFont('Arial',20))
         #image = QPixmap(r'C:\Users\Lenovo\Desktop\prj_PyQt\logo.png').scaled(300, 300, Qt.AspectRatioMode.KeepAspectRatio)
         #self.label.setPixmap(image)
         #self.label.resize(300, 300)
-        self.button = QPushButton('Qpushbutton', self)
-        self.button.setGeometry(200, 10, 100, 50)
-        self.button.setFont(QFont('Aria', 5))
-        self.button.setIcon(QIcon('logo.png'))
+        # self.button = QPushButton('Qpushbutton', self)
+        # self.button.setGeometry(200, 10, 100, 50)
+        # self.button.setFont(QFont('Aria', 5))
+        # self.button.setIcon(QIcon('logo.png'))
 
 
-        self.create_widget()
+       # self.create_widget()
     #     self.create_qline_edit()
     #     self.clicked()
 
@@ -42,22 +43,37 @@ class MyWindow(QWidget):
     #         self.label.setText(self.input_text.text())
     #         self.input_text.clear()
 
-    def create_widget(self):
-        btn1=QPushButton('btn1')
-        btn2=QPushButton('btn2')
-        btn3=QPushButton('btn3')
-        btn4=QPushButton('btn4')
+    # def create_widget(self):
+    #     btn1=QPushButton('btn1')
+    #     btn2=QPushButton('btn2')
+    #     btn3=QPushButton('btn3')
+    #     btn4=QPushButton('btn4')
 
-        hbox=QHBoxLayout()
+    #     hbox=QHBoxLayout()
 
-        hbox.addWidget(btn1)
-        hbox.addWidget(btn2)
-        hbox.addWidget(btn3)
-        hbox.addWidget(btn4)
-        hbox.addSpacing(100)
-        hbox.setStretch(10 ,10)
+    #     hbox.addWidget(btn1)
+    #     hbox.addWidget(btn2)
+    #     hbox.addWidget(btn3)
+    #     hbox.addWidget(btn4)
+    #     hbox.addSpacing(100)
+    #     hbox.setStretch(10 ,10)
 
-        self.setLayout(hbox)
+    #     self.setLayout(hbox)
+        self.lcd =QLCDNumber()
+        self.lcd.setStyleSheet('background:blue')
+        self.hbox=QHBoxLayout()
+        self.hbox.addWidget(self.lcd)
+        self.setLayout(self.hbox)
+        self.show_time()
+        self.timer=QTimer()
+        self.timer.timeout.connect(self.show_time)
+        self.timer.start(1000)
+    def show_time(self):
+        time=QTime.currentTime()
+        show_time = time.toString('mm:ss')
+        self.lcd.display(show_time)
+
+
 
 
 app=QApplication(sys.argv)
